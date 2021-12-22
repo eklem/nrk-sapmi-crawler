5ae2c3633b79206a1a6e4d187421ada1a31a0773
Espen Klem <espen.klem@gmail.com>
2021-12-22 22:32:05 +0100

    Returning both promises - fetching JSON from URL and reading file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
-import * as fs from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
+const url = 'https://www.nrk.no/serum/api/content/json/1.13572943?v=2&limit=1000&context=items'
+const file = './list.json'
 
 async function getList (url) {
   const response = await fetch(url)
@@ -7,8 +9,16 @@ async function getList (url) {
   return data
 }
 
-getList('https://www.nrk.no/serum/api/content/json/1.13572943?v=2&limit=1000&context=items')
-  .then((data) => console.log(data))
+async function readIfExists (fileName) {
+  try {
+    const file = JSON.parse(await readFile(fileName))
+    return file
+  } catch (err) {
+    // If i.e. file doesn't exists
+    console.error('File doesn\'t exist? Error: ' + err)
+    // create file
+  }
+}
 
-// Check if file exists function
-// https://stackoverflow.com/questions/17699599/node-js-check-if-file-exists
+Promise.all([getList(url), readIfExists(file).catch(e => e)])
+  .then((data) => console.log(data[0]))
