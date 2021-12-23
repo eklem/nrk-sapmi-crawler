dd3a57be24a5c69646d2e96f845aaacf65920deb
Espen Klem <espen.klem@gmail.com>
2021-12-23 14:24:57 +0100

    scaffold for calculate and write file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import fetch from 'node-fetch'
 import { readFile, writeFile } from 'fs/promises'
-const url = 'https://www.nrk.no/serum/api/content/json/1.13572943?v=2&limit=1000&context=items'
-const file = './list.json'
+const parentId = 1.13572943
+const url = 'https://www.nrk.no/serum/api/content/json/' + parentId + '?v=2&limit=1000&context=items'
+const fileName = './list.json'
+let startFromScratch = false
 
 // Get list of article IDs from NRK
 async function getList (url) {
@@ -23,9 +25,36 @@ async function readIfExists (fileName) {
     // If i.e. file doesn't exists
     console.error('File doesn\'t exist? Error: ' + err)
     // create file
+    startFromScratch = true
   }
 }
 
+/*
+Array of object format:
+[
+  {
+    "id": "1.15778840",
+    "parent-id": "1.13572943",
+    "crawled": false
+  }
+]
+The array can be picked from data[0].relations
+*/
+async function calculateListAndWrite (data, startFromScratch) {
+  // weave together if necessary
+  if (startFromScratch) {
+    console.log('start from scratch: ' + startFromScratch)
+    // just create object from JSON fetch
+  } else {
+    // weave togheter
+    console.log('start from scratch: ' + startFromScratch)
+  }
+  // write to file
+}
+
 // Bringing it all together, fetching URL and reading file
-Promise.all([getList(url), readIfExists(file).catch(e => e)])
-  .then((data) => console.log('all good'))
+Promise.all([getList(url), readIfExists(fileName).catch(e => e)])
+  .then((data) => {
+    console.log(data[0].relations)
+    calculateListAndWrite(data, startFromScratch)
+  })
