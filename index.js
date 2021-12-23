498dd1c38008cde899dc85e38601ee4118d6d622
Espen Klem <espen.klem@gmail.com>
2021-12-23 15:40:17 +0100

    Writing file with list of IDs

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import fetch from 'node-fetch'
 import { readFile, writeFile } from 'fs/promises'
 const parentId = 1.13572943
 const url = 'https://www.nrk.no/serum/api/content/json/' + parentId + '?v=2&limit=1000&context=items'
-const fileName = './list.json'
+const fileName = './lib/list.' + parentId + '.json'
 let startFromScratch = false
+const IdArray = []
 
 // Get list of article IDs from NRK
 async function getList (url) {
@@ -29,32 +30,49 @@ async function readIfExists (fileName) {
   }
 }
 
-/*
-Array of object format:
-[
-  {
-    "id": "1.15778840",
-    "parent-id": "1.13572943",
-    "crawled": false
-  }
-]
-The array can be picked from data[0].relations
-*/
-async function calculateListAndWrite (data, startFromScratch) {
+// calculate array of objects to write
+// write it
+async function calculateListAndWrite (data, startFromScratch, IdArray, parentId, fileName) {
   // weave together if necessary
   if (startFromScratch) {
     console.log('start from scratch: ' + startFromScratch)
     // just create object from JSON fetch
+    /*
+      let reformattedArray = kvArray.map(obj => {
+        let rObj = {}
+        rObj[obj.key] = obj.value
+        return rObj
+      })
+    */
+    IdArray = data[0].relations.map(obj => {
+      const newObj = {}
+      newObj.id = obj.id
+      newObj.parentId = parentId
+      newObj.crawled = false
+      return newObj
+    })
+    console.log(IdArray)
   } else {
     // weave togheter
     console.log('start from scratch: ' + startFromScratch)
   }
   // write to file
+  try {
+    const promise = writeFile(fileName, JSON.stringify(IdArray))
+    await promise
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 // Bringing it all together, fetching URL and reading file
 Promise.all([getList(url), readIfExists(fileName).catch(e => e)])
   .then((data) => {
-    console.log(data[0].relations)
-    calculateListAndWrite(data, startFromScratch)
+    // console.log(data[0].relations)
+    console.log(data[0])
+    calculateListAndWrite(data, startFromScratch, IdArray, parentId, fileName)
+  })
+  .catch(function (err) {
+    // rejection
+    console.log('Error: ' + err)
   })
