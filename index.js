ae374724c075dcab8019051a947655b26adf53c1
Espen Klem <espen.klem@gmail.com>
2021-12-23 05:50:30 +0100

    picking up http status code + text for later handling

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import { readFile, writeFile } from 'fs/promises'
 const url = 'https://www.nrk.no/serum/api/content/json/1.13572943?v=2&limit=1000&context=items'
 const file = './list.json'
 
+// Get list of article IDs from NRK
 async function getList (url) {
   const response = await fetch(url)
+  // console.log(response)
   const data = await response.json()
+  console.log('Response Code: ', response.status)
+  console.log('Response Message: ', response.statusText)
   return data
 }
 
+// Read local file with already stored article IDs
+// If it doesn't exist, it's created
 async function readIfExists (fileName) {
   try {
     const file = JSON.parse(await readFile(fileName))
@@ -20,5 +26,6 @@ async function readIfExists (fileName) {
   }
 }
 
+// Bringing it all together, fetching URL and reading file
 Promise.all([getList(url), readIfExists(file).catch(e => e)])
-  .then((data) => console.log(data[0]))
+  .then((data) => console.log('all good'))
