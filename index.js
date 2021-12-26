6e0ac438fd6a8504a3d7ceb76f1dd74c6a05b341
Espen Klem <espen.klem@gmail.com>
2021-12-26 19:43:28 +0100

    Merging crawled and read if needed and sorting

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import { readFile, writeFile } from 'fs/promises'
 const parentId = 1.13572943
 const url = 'https://www.nrk.no/serum/api/content/json/' + parentId + '?v=2&limit=1000&context=items'
 const fileName = './lib/list.' + parentId + '.json'
-let startFromScratch = false
-const IdArray = []
+let startingFromScratch = false
+const crawledIds = []
+const IdsToWrite = []
 
 // Get list of article IDs from NRK
 async function getList (url) {
@@ -20,57 +21,69 @@ async function getList (url) {
 // If it doesn't exist, it's created
 async function readIfExists (fileName) {
   try {
-    const file = JSON.parse(await readFile(fileName))
-    return file
+    const data = JSON.parse(await readFile(fileName))
+    return data
   } catch (err) {
     // If i.e. file doesn't exists
     console.error('File doesn\'t exist? Error: ' + err)
-    // create file
-    startFromScratch = true
+    // used in function calculateListAndWrite
+    startingFromScratch = true
   }
 }
 
 // calculate array of objects to write
 // write it
-async function calculateListAndWrite (data, startFromScratch, IdArray, parentId, fileName) {
+async function calculateListAndWrite (data, startingFromScratch, crawledIds, IdsToWrite, parentId, fileName) {
+  crawledIds = data[0].relations.map(obj => {
+    const newObj = {}
+    newObj.id = Number(obj.id)
+    newObj.unixTime = new Date(obj.created).getTime()
+    newObj.parentId = parentId
+    newObj.crawled = false
+    return newObj
+  })
+  // console.log(crawledIds)
+
   // weave together if necessary
-  if (startFromScratch) {
-    console.log('start from scratch: ' + startFromScratch)
-    // just create object from JSON fetch
-    /*
-      let reformattedArray = kvArray.map(obj => {
-        let rObj = {}
-        rObj[obj.key] = obj.value
-        return rObj
-      })
-    */
-    IdArray = data[0].relations.map(obj => {
-      const newObj = {}
-      newObj.id = obj.id
-      newObj.parentId = parentId
-      newObj.crawled = false
-      return newObj
+  if (!startingFromScratch) {
+    console.log('start from scratch: ' + startingFromScratch)
+    // console.log(data)
+    // Go through crawledIds and push objects to array
+    IdsToWrite = data[1]
+    crawledIds.forEach(crawledObj => {
+      if (data[1].some(readObj => readObj.id === crawledObj.id)) {
+        console.log(crawledObj.id + ' already in readObj')
+      } else {
+        IdsToWrite.push(crawledObj)
+      }
     })
-    console.log(IdArray)
+    // Sort on ID
+    IdsToWrite.sort((secondItem, firstItem) => firstItem.id - secondItem.id)
   } else {
-    // weave togheter
-    console.log('start from scratch: ' + startFromScratch)
+    IdsToWrite = crawledIds
   }
   // write to file
   try {
-    const promise = writeFile(fileName, JSON.stringify(IdArray))
+    const promise = writeFile(fileName, JSON.stringify(IdsToWrite))
     await promise
   } catch (err) {
     console.error(err)
   }
 }
 
+// if object not included in array -> true/false
+// Do something like this: cases.hitFound = itemArr.some(word => queryArr.includes(word))
+// ... or check this one: https://www.tutorialrepublic.com/faq/how-to-check-if-an-array-includes-an-object-in-javascript.php
+function notIncludedInArray () {
+  // if
+}
+
 // Bringing it all together, fetching URL and reading file
 Promise.all([getList(url), readIfExists(fileName).catch(e => e)])
   .then((data) => {
     // console.log(data[0].relations)
-    console.log(data[0])
-    calculateListAndWrite(data, startFromScratch, IdArray, parentId, fileName)
+    // console.log(data[0])
+    calculateListAndWrite(data, startingFromScratch, crawledIds, IdsToWrite, parentId, fileName)
   })
   .catch(function (err) {
     // rejection
