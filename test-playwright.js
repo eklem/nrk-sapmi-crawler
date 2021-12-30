5cda139cd4038b7dfd77040f2866af13d997a574
Espen Klem <espen.klem@gmail.com>
2021-12-30 23:03:16 +0100

    Handling images missing, sort of Sould do a check and then probe for more properties

--- test-playwright.js
+++ test-playwright.js
@@ -1,7 +1,8 @@
 
 import { chromium } from 'playwright'
-const crawlObject = { id: '1.15778840', unixTime: 1639919176000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
+// const crawlObject = { id: '1.15778840', unixTime: 1639919176000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
 // const crawlObject = { id: '1.15761886', unixTime: 1638894940000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
+const crawlObject = { id: '1.15789637', unixTime: 1640782941000, languageId: '1.13572949', languageName: 'Davvisámegillii', crawled: false }
 const aLittleTime = 1000
 const urlStart = 'https://nrk.no/'
 
@@ -20,9 +21,9 @@ async function crawl (url) {
     element.map((e) => e.innerText)
   )
   crawlContent.img = {}
-  crawlContent.img.srcset = await page.$eval('div.responsive-img img', img => img.srcset)
-  crawlContent.img.description = await page.$eval('div.responsive-img img', img => img.alt)
-  crawlContent.img.credit = await page.$eval('div.responsive-img img', img => img.title)
+  crawlContent.img.srcset = await page.$eval('figure.bulletin-attachment > div.responsive-img img', img => img.srcset).catch(console.error)
+  crawlContent.img.description = await page.$eval('figure.bulletin-attachment > div.responsive-img img', img => img.alt).catch(console.error)
+  crawlContent.img.credit = await page.$eval('figure.bulletin-attachment > div.responsive-img img', img => img.title).catch(console.error)
   crawlContent.year = await page.textContent('span.bulletin-publish-year')
   crawlContent.unixTime = crawlObject.unixTime
   crawlContent.languageName = crawlObject.languageName
