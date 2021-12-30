b0b53ef57f42412f6c4dc8a3596009c071446d12
Espen Klem <espen.klem@gmail.com>
2021-12-30 22:36:56 +0100

    small adjustment to img object

--- test-playwright.js
+++ test-playwright.js
@@ -1,19 +1,19 @@
 
 import { chromium } from 'playwright'
-// const crawlObject = { id: '1.15778840', unixTime: 1639919176000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
-const crawlObject = { id: '1.15761886', unixTime: 1638894940000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
+const crawlObject = { id: '1.15778840', unixTime: 1639919176000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
+// const crawlObject = { id: '1.15761886', unixTime: 1638894940000, languageId: '1.13572943', languageName: 'Åarjelsaemien', crawled: false }
 const aLittleTime = 1000
 const urlStart = 'https://nrk.no/'
 
 async function crawl (url) {
   const browser = await chromium.launch({
-    headless: true // setting this to true will not run the UI
+    headless: true
   })
   const crawlContent = {}
 
   const page = await browser.newPage()
   await page.goto(url)
-  await page.waitForTimeout(aLittleTime) // wait for 5 seconds
+  await page.waitForTimeout(aLittleTime)
   crawlContent.url = await page.url()
   crawlContent.title = await page.textContent('h2.bulletin-title')
   crawlContent.body = await page.$$eval('div.bulletin-text p', (element) =>
@@ -21,8 +21,8 @@ async function crawl (url) {
   )
   crawlContent.img = {}
   crawlContent.img.srcset = await page.$eval('div.responsive-img img', img => img.srcset)
-  crawlContent.img.text = await page.$eval('div.responsive-img img', img => img.alt)
-  crawlContent.img.photocredit = await page.$eval('div.responsive-img img', img => img.title)
+  crawlContent.img.description = await page.$eval('div.responsive-img img', img => img.alt)
+  crawlContent.img.credit = await page.$eval('div.responsive-img img', img => img.title)
   crawlContent.year = await page.textContent('span.bulletin-publish-year')
   crawlContent.unixTime = crawlObject.unixTime
   crawlContent.languageName = crawlObject.languageName
